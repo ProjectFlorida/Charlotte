921690ada3550aee1b89b82f19f5327c7d02302b
Ben Guo <benzguo@gmail.com>
2014-12-09 15:11:36 -0500

    update scrollToPage and expose currentPage

---
+++ Examples/Examples/EndlessBarChartViewController.h
@@ -0,0 +1,13 @@
+//
+//  EndlessBarChartViewController.h
+//  Examples
+//
+//  Created by Ben Guo on 12/9/14.
+//  Copyright (c) 2014 Project Florida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EndlessBarChartViewController : UIViewController
+
+@end
